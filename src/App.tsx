dacc383e2c54a5b92bce4c6e108c0ecdc4c83391
junihoj <ezeonyekachukwu98@gmail.com>
2026-03-02 16:02:48 +0100

    chores: project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavBar from "#components/layouts/nav-bar";
+
+const App = () => {
+  return (
+    <main className="">
+      <NavBar />
+      <h1 className="text-white text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/layouts/nav-bar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
